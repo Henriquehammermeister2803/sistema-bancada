ebdc79277adfc475c7f75b10115e178c654f625f
HENRIQUE JOSE HAMMERMEISTER <henrique_hammerme@estudante.sesisenai.org.br>
2026-03-05 17:02:46 -0300

    bancada

--- frontend/index.js
+++ frontend/index.js
@@ -10,27 +10,3 @@ function login() {
   alert("Login feito com sucesso!\nUsuário: " + user);
   window.location.href = 'bancada.html';
 }
-
-function cadastrar() {
-  const usuario = document.getElementById("cadUser").value;
-  const email = document.getElementById("cadEmail").value;
-  const data = document.getElementById("cadData").value;
-  const cidade = document.getElementById("cadCidade").value;
-  const senha = document.getElementById("cadSenha").value;
-
-  if (usuario === "" || email === "" || data === "" || cidade === "" || senha === "") {
-    alert("Preencha todos os campos!");
-    return;
-  }
-
-  alert(
-    "Cadastro realizado com sucesso!\n\n" +
-    "Usuário: " + usuario +
-    "\nE-mail: " + email +
-    "\nNascimento: " + data +
-    "\nCidade: " + cidade
-  );
-
-  // Volta para login
-  window.location.href = "index.html";
-}
